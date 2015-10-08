ff068eafc472f186a7a1cf1b14b99a0eaf5a4a3f
mmsergi <sergimrtnz@gmail.com>
2015-10-08 10:43:06 +0200

    gas

---
+++ config.lua
@@ -0,0 +1,12 @@
+application = {
+
+    content = {
+        scale = "letterbox",
+        width = 1080,
+        height = 1920, 
+        fps = 24,
+        xAlign = "center",
+        yAlign = "center"
+    },
+    
+}
---
+++ menu.lua
@@ -0,0 +1,83 @@
+local scene = composer.newScene()
+
+-- All code outside of the listener functions will only be executed ONCE unless "composer.removeScene()" is called.
+-- local forward references should go here
+
+
+
+
+function scene:create( event )
+
+    local group = self.view
+
+    -- Initialize the scene here.
+    -- Example: add display objects to "group", add touch listeners, etc.
+    -- "scene:create()"
+end
+
+
+
+function scene:show( event )
+
+    local group = self.view
+    local phase = event.phase
+
+    if ( phase == "will" ) then
+        -- Called when the scene is still off screen (but is about to come on screen).
+
+
+
+
+    elseif ( phase == "did" ) then
+        -- Called when the scene is now on screen.
+        -- Insert code here to make the scene come alive.
+        -- Example: start timers, begin animation, play audio, etc.
+
+
+
+    end-- "scene:show()"
+end-- "scene:show()"
+
+
+
+function scene:hide( event )
+
+    local group = self.view
+    local phase = event.phase
+
+    if ( phase == "will" ) then
+        -- Called when the scene is on screen (but is about to go off screen).
+        -- Insert code here to "pause" the scene.
+        -- Example: stop timers, stop animation, stop audio, etc.
+
+    elseif ( phase == "did" ) then
+        -- Called immediately after scene goes off screen.
+
+    end-- "scene:hide()"
+end-- "scene:hide()"
+
+
+
+function scene:destroy( event )
+
+    local group = self.view
+
+    -- Called prior to the removal of scene's view ("group").
+    -- Insert code here to clean up the scene.
+    -- Example: remove display objects, save state, etc.
+
+
+end-- "scene:destroy()"
+
+
+-- -------------------------------------------------------------------------------
+
+
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+
+-- -------------------------------------------------------------------------------
+
+return scene
