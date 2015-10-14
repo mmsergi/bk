7904a248feb4e4c57ea8379ae0778d65c70813c2
Sergi <sergimrtnz@gmail.com>
2015-10-14 13:14:07 +0200

    fa algu
    ye

--- war.lua
+++ war.lua
@@ -1,17 +1,76 @@
 local scene = composer.newScene()
 local StickLib   = require("scripts.lib_analog_stick")
+local localGroup
 system.activate( "multitouch" )
+physics.start( )
+physics.setDrawMode( "hybrid" )
+physics.setGravity( 0, 0 )
 -- All code outside of the listener functions will only be executed ONCE unless "composer.removeScene()" is called.
 -- local forward references should go here
 
+local function checkGoldCollision(gold, char)
+    local incxpow = math.pow( gold.x - char.x, 2 ) 
+    local incypow = math.pow( gold.y - char.y, 2 ) 
+    local d = math.sqrt(incxpow+incypow)
+    return d
+end
+
+-- Local collision handling
+local function onGoldCollision(gold, event)
+    if (event.other.id == "hero1") then
+        hero1.state = "goldOn"
+        goldOnHero1 = display.newImage("images/gold.png", hero1.x, hero1.y - 10)  
+        gold:removeSelf( )     
+    elseif (event.other.id == "hero2") then
+        hero2.state = "goldOn"      
+        goldOnHero2 = display.newImage("images/gold.png", hero2.x, hero2.y - 10)
+        gold:removeSelf( )   
+    end
+end
+
+local function positionGold(posX, posY)
+    gold = display.newImage("images/gold.png", posX, posY)
+    physics.addBody( gold )
+    gold.collision = onGoldCollision
+    gold:addEventListener( "collision", gold )
+end
+
+-- Global collision handling
+local function onGlobalCollision( event )
+    if (event.object1.id=="hero1" and event.object2.id=="hero2") then
+        print("heros collision")
+        if (hero1.state=="goldOn" and hero2.state==nil) then
+            print("hero 2 attack")
+            goldOnHero1:removeSelf( )
+            hero1.state=nil
+            timer.performWithDelay(20, function() positionGold(hero1.x - 100, hero1.y) end)
+        elseif (hero1.state==nil and hero2.state=="goldOn") then
+            print("hero 1 attack")
+            goldOnHero2:removeSelf( )
+            hero2.state=nil
+            timer.performWithDelay(20, function() positionGold(hero2.x - 100, hero2.y) end)
+        end
+    end
+end
+
+
+local function frameUpdates ()
+    if (goldOnHero1~=nil) then
+        goldOnHero1.x, goldOnHero1.y = hero1.x, hero1.y - 10
+    end
+
+    if (goldOnHero2~=nil) then
+        goldOnHero2.x, goldOnHero2.y = hero2.x, hero2.y - 10
+    end
+end
+        
 function scene:create( event )
-
-    local localGroup = self.view
-
+    localGroup = self.view
+    
     local Text = display.newText( " ", _W*.6, _H-20, native.systemFont, 15 )
 
     -- local hero
-    local localGroup = display.newGroup() -- remember this for farther down in the code
+    localGroup = display.newGroup() -- remember this for farther down in the code
     motionx = 0; -- Variable used to move character along x axis
     motiony = 0; -- Variable used to move character along y axis
     speed = 2; -- Set Walking Speed 
@@ -19,9 +78,9 @@ function scene:create( event )
     -- CREATE ANALOG STICK
     MyStick = StickLib.NewStick( 
             {
-            x             = leftMarg+50,
+            x             = cx,
             y             = bottomMarg-50,
-            thumbSize     = 32,
+            thumbSize     = 16,
             borderSize    = 32, 
             snapBackSpeed = .2, 
             R             = 255,
@@ -31,9 +90,9 @@ function scene:create( event )
 
     MyStick2 = StickLib.NewStick( 
             {
-            x             = rightMarg - 50,
+            x             = cx,
             y             = topMarg + 50,
-            thumbSize     = 32,
+            thumbSize     = 16,
             borderSize    = 32, 
             snapBackSpeed = .2, 
             R             = 255,
@@ -45,7 +104,7 @@ function scene:create( event )
     local function main( event )
             
         -- MOVE THE SHIP
-        MyStick:move(hero, speed, false) -- se a opção for true o objeto se move com o joystick
+        MyStick:move(hero1, speed, false) -- se a opção for true o objeto se move com o joystick
 
         MyStick2:move(hero2, speed, false) -- se a opção for true o objeto se move com o joystick
 
@@ -94,8 +153,8 @@ function scene:create( event )
         end
         
         --Change the sequence only if another sequence isn't still playing 
-        if(not (seq == hero.sequence) and moving) then -- and not attacking
-            hero:setSequence(seq)
+        if(not (seq == hero1.sequence) and moving) then -- and not attacking
+            hero1:setSequence(seq)
         end
         
         if(not (seq2 == hero2.sequence) and moving2) then -- and not attacking
@@ -104,7 +163,7 @@ function scene:create( event )
 
         --If the analog stick is moving, animate the sprite
         if(moving) then 
-            hero:play() 
+            hero1:play() 
         end
 
             --If the analog stick is moving, animate the sprite
@@ -122,22 +181,26 @@ function scene:create( event )
     bg.x = cx
     bg.y = cy
 
-
     -- Display the new sprite at the coordinates passed
-    hero = display.newSprite(mySheet, sequenceData)
-    hero:setSequence("forward")
-    hero.x = cx
-    hero.y = cy + 30
+    hero1 = display.newSprite(localGroup, mySheet, sequenceData)
+    hero1:setSequence("forward")
+    hero1.x = cx
+    hero1.y = cy + 30
+    hero1.id = "hero1"
+    physics.addBody( hero1, { density=1.0, friction=0.3, bounce=0.2, radius=25 } )
 
-    hero2 = display.newSprite(mySheet, sequenceData)
+    hero2 = display.newSprite(localGroup, mySheet, sequenceData)
     hero2:setSequence("back")
     hero2.x = cx
     hero2.y = cy - 30
+    hero2.id = "hero2"
+    physics.addBody( hero2, { density=1.0, friction=0.3, bounce=0.2, radius=25 } )
 
+    gold = display.newImage(localGroup, "images/gold.png", cx, cy + 120 )
+    physics.addBody( gold )
+    gold.collision = onGoldCollision
+    gold:addEventListener( "collision", gold )
 
-    localGroup:insert(bg)
-    localGroup:insert(hero)
-    localGroup:insert(hero2)
 
 end-- "scene:create()"
 
@@ -155,8 +218,8 @@ function scene:show( event )
         -- Called when the scene is now on screen.
         -- Insert code here to make the scene come alive.
         -- Example: start timers, begin animation, play audio, etc.
-
-
+        Runtime:addEventListener( "enterFrame", frameUpdates )
+        Runtime:addEventListener( "collision", onGlobalCollision )
     end-- "scene:show()"
 end-- "scene:show()"
 
